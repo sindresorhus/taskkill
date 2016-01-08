cd9acac4ec6512d968bd8812374c26cf9e678991
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-08 16:05:50 +0100

    Require Node.js >=4

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
-var arrify = require('arrify');
-var execa = require('execa');
-var Promise = require('pinkie-promise');
+const arrify = require('arrify');
+const execa = require('execa');
 
-module.exports = function (input, opts) {
-	opts = opts || {};
+module.exports = (input, opts) => {
 	input = arrify(input);
+	opts = opts || {};
 
 	if (process.platform !== 'win32') {
 		return Promise.reject(new Error('Windows only'));
@@ -15,7 +14,7 @@ module.exports = function (input, opts) {
 		return Promise.reject(new Error('PID or image name required'));
 	}
 
-	var args = [];
+	const args = [];
 
 	if (opts.system && opts.username && opts.password) {
 		args.push('/s', opts.system, '/u', opts.username, '/p', opts.password);
@@ -33,9 +32,7 @@ module.exports = function (input, opts) {
 		args.push('/t');
 	}
 
-	input.forEach(function (el) {
-		args.push(typeof el === 'number' ? '/pid' : '/im', el);
-	});
+	input.forEach(x => args.push(typeof x === 'number' ? '/pid' : '/im', x));
 
 	return execa('taskkill', args);
 };
