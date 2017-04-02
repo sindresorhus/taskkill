0acca6a5232e2c2ab8cb341efb55a1ff6630a7ac
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2017-04-03 00:40:45 +0200

    Add throws on not found test

--- test.js
+++ test.js
@@ -2,10 +2,14 @@ import childProcess from 'child_process';
 import test from 'ava';
 import m from './';
 
-test(async t => {
+test('kills a process', async t => {
 	const pid = childProcess.spawn(process.execPath).pid;
 	await m(pid, {force: true});
 
 	// Check if the process exists
 	t.throws(() => process.kill(pid));
+});
+
+test('throws on not found', t => {
+	t.throws(m('not-running.exe'));
 });
