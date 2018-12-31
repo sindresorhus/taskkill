37315fdd012264d7dc7d6507bc5c2e7ef3809ab0
Brandon Smith <brandon93s@gmail.com>
2018-12-30 20:21:55 -0500

    CI updates (#9)

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 import childProcess from 'child_process';
 import test from 'ava';
-import m from './';
+import m from '.';
 
 test('kills a process', async t => {
-	const pid = childProcess.spawn(process.execPath).pid;
+	const {pid} = childProcess.spawn(process.execPath);
 	await m(pid, {force: true});
 
 	// Check if the process exists
 	t.throws(() => process.kill(pid));
 });
 
-test('throws on not found', t => {
-	t.throws(m('not-running.exe'));
+test('throws on not found', async t => {
+	await t.throwsAsync(m('not-running.exe'));
 });
